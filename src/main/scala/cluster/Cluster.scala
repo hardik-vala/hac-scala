e521848cabff1e59287384eef2f14db4450f73bb
Hardik Vala <hardikvala24@gmail.com>
2016-06-03 11:26:40 -0400

    Created src directory and Cluster

---
+++ src/main/scala/cluster/Cluster.scala
@@ -0,0 +1,20 @@
+package cluster
+
+/**
+  * Cluster.
+  *
+  * @param id - Simple integer id for the cluster (for tracking purposes during clustering).
+  * @param elements - Cluster elements.
+  * @tparam T - Type of elements.
+  */
+case class Cluster[T](id: Int, elements: Iterable[T]) {
+
+  /**
+    * Cluster size.
+    *
+    * @return # of cluster elements.
+    */
+  def size: Int = this.elements.size
+
+}
+
